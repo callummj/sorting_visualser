600eb49f597cc375edf782416bd95f210e73b540
Callum <callumj22@gmail.com>
2020-10-13 13:25:19 +0100

    Using the Data.state, creates a centred <div> which then contains a child div, filled with a background colour, representing the data generated. CJ 13/10/20

--- src/components/Visualiser.js
+++ src/components/Visualiser.js
@@ -21,10 +21,9 @@ export default class Visualiser extends React.Component{
         this.generateData();
     }
 
+    //Generates Data & sets the Data state to its value
     generateData = () =>{
-
         let dataTemp = [];
-
         for (let i = 0; i < 20; i++){
             dataTemp.push(Math.floor(Math.random() * 100)+1);
         }
@@ -34,24 +33,29 @@ export default class Visualiser extends React.Component{
         });
     }
 
-    dataToBars = () =>{
-        var element = '<div>'
 
-        for (let i = 0; i < this.state.data.length; i++){
-            element +=
-                <div
-                className="array-bar">
+    //Creates a bar representing each piece of data in the array, using CSS capsulated in a <div>
+    dataToBars = () => {
+        console.log("data to bars")
+        return (
+            <div>
+                {
+                    this.state.data.map(i => (
+                        <div className="arraybar"  style={{
+                            height: `${i}px`,
+                        }}/>
+                    ))}
+            </div>
+        );
 
-                </div>
-        }
 
-        element += '</div>';
-
-        console.log("element: " + element);
-        return element;
     }
 
     render() {
+
+        //Renders a test generate button, and displays the data generated above.
+        //TODO Make the generate button in the Toolbar work.
+
         return(
             <div>
 
@@ -59,9 +63,11 @@ export default class Visualiser extends React.Component{
 
                 <p>{this.state.data}</p>
 
-                <div className={'arrayArea'}>
-                    <p>{this.dataToBars}</p>
+
+                <div className = "array-container">
+                    {this.dataToBars()}
                 </div>
+
 
                 <p>test</p>
             </div>
