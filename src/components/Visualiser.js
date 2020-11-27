70944e4cc5c09f0b999b1e7880b5c3d074ac975f
Callum <callumj22@gmail.com>
2020-11-27 16:03:41 +0000

    implemented ability to clear the bars, in progress: making the second set of bars animate the second algorithm.

--- src/components/Visualiser.js
+++ src/components/Visualiser.js
@@ -22,6 +22,11 @@ export default class Visualiser extends React.Component{
 
         this.state = {
             data: [], //initialises data state
+            algorithms: [],
+            insertionSortData: [],
+            quickSortData: [],
+            bubbleSortData: [],
+            mergeSortData: [],
         }
 
     }
@@ -43,6 +48,12 @@ export default class Visualiser extends React.Component{
 
         this.setState({
             data: dataTemp,
+            algorithms: [],
+            insertionSortData: [],
+            quickSortData: [],
+            bubbleSortData: [],
+            mergeSortData: [],
+
         });
 
 
@@ -52,31 +63,78 @@ export default class Visualiser extends React.Component{
 
 
     //Creates a bar representing each piece of data in the array, using CSS capsulated in a <div>
-    dataToBars = () => {
+    dataToBars = (algorithm) => {
 
-        return( <div id = "bars">
-            {
-                this.state.data.map(i => (
-                    <div key = {Math.random() * 100} className="arraybar"  style={{
-                        height: `${i}px`,
-                    }}/>
-                ))}
-        </div>)
+        console.log("algorithm:" + algorithm)
+        switch (algorithm){
+            case "quickSort":
+                return( <div id = "quicksort-bars">
+                    {
+                        this.state.quickSortData.map(i => (
+                            <div key = {Math.random() * 100} className="arraybar"  style={{
+                                height: `${i}px`,
+                            }}/>
+                        ))}
+                </div>)
 
+                break;
+            case "bubbleSort":
+                return( <div id = "bubblesort-bars">
+                    {
+                        this.state.bubbleSortData.map(i => (
+                            <div key = {Math.random() * 100} className="arraybar"  style={{
+                                height: `${i}px`,
+                            }}/>
+                        ))}
+                </div>)
+                break;
+            case "insertionSort":
+
+                return( <div id = "insertionsort-bars">
+                    {
+                        this.state.insertionSortData.map(i => (
+                            <div key = {Math.random() * 100} className="arraybar"  style={{
+                                height: `${i}px`,
+                            }}/>
+                        ))}
+                </div>)
+                break;
+            case "mergeSort":
+                return( <div id = "bars">
+                    {
+                        this.state.mergeSortData.map(i => (
+                            <div key = {Math.random() * 100} className="arraybar"  style={{
+                                height: `${i}px`,
+                            }}/>
+                        ))}
+                </div>)
+                break;
+            default:
+                return( <div id = "bars">
+                    {
+                        this.state.data.map(i => (
+                            <div key = {Math.random() * 100} className="arraybar"  style={{
+                                height: `${i}px`,
+                            }}/>
+                        ))}
+                </div>)
+                break;
         }
 
+
+    }
 
 
 
 
     quickSortHandler = () =>{
 
-
+        this.addAlgorithm("quickSort");
         let parent = this;
-        let steps = Quicksort ((this.state.data), 0, (this.state.data.length-1));
+        let steps = Quicksort ((this.state.quickSortData), 0, (this.state.quickSortData.length-1));
 
         for (let i = 0; i < steps.length; i++){
-            setTimeout(()=>(this.update(steps[i]), console.log(steps[i])), 100*i);
+            setTimeout(()=>(this.update(steps[i], "quickSort"), console.log(steps[i])), 100*i);
         }
 
 
@@ -84,85 +142,88 @@ export default class Visualiser extends React.Component{
 
 
     insertionSortHandler = () =>{
-
+        this.addAlgorithm("insertionSort")
         let steps = [];
         let parent = this;
         //steps = parent.insertionSort(steps);
-        console.log("insertionsort data: " + this.state.data)
-        steps = Insertionsort(this.state.data);
+        console.log("insertionsort data: " + this.state.insertionSortData)
+        steps = Insertionsort(this.state.insertionSortData);
         for (let i = 0; i < steps.length; i++){
-            setTimeout(()=>(parent.update(steps[i]), console.log(steps[i])), 100*i);
+            setTimeout(()=>(parent.update(steps[i], "insertionSort"), console.log(steps[i])), 100*i);
         }
 
     }
 
     bubbleSortHandler = () =>{
-
+        this.addAlgorithm("bubbleSort")
         let steps = [];
         let parent = this;
-        steps = Bubblesort(this.state.data);
+        console.log("here")
+        steps = Bubblesort(this.state.bubbleSortData);
+        console.log("here2")
         for (let i = 0; i < steps.length; i++){
-            setTimeout(()=>(parent.update(steps[i]), console.log(steps[i])), 100*i);
+            setTimeout(()=>(parent.update(steps[i], "bubbleSort"), console.log(steps[i])), 100*i);
         }
+        console.log("here4")
     }
-/*
-    mergeSortHandler = () =>{
-        let steps = [];
-        let parent = this;
-        steps = MergeSort(this.state.data);
-        for (let i = 0; i < steps.length; i++){
-            setTimeout(()=>(parent.update(steps[i]), console.log(steps[i])), 100*i);
+    /*
+        mergeSortHandler = () =>{
+            let steps = [];
+            let parent = this;
+            steps = MergeSort(this.state.MergeSortData);
+            for (let i = 0; i < steps.length; i++){
+                setTimeout(()=>(parent.update(steps[i]), console.log(steps[i])), 100*i);
+            }
         }
-    }
 
-*/
+    */
 
 
     mergeSortHandler = () =>{
-
+        this.addAlgorithm("mergeSort")
         let steps = [];
         let parent = this;
         let result = [];
-        steps = MergeSortDriver(this.state.data, result);
+        steps = MergeSortDriver(this.state.mergeSortData, result);
         for (let i = 0; i < steps.length; i++){
-            setTimeout(()=>(parent.update(steps[i]), console.log(steps[i])), 100*i);
+            setTimeout(()=>(parent.update(steps[i], "mergeSort"), console.log(steps[i])), 100*i);
         }
     }
 
-/*
-    mergeSort(arr){
-        var len = arr.length;
-        if(len <2)
-            return arr;
-        var mid = Math.floor(len/2);
-        var left = arr.slice(0,mid);
-        var right =arr.slice(mid);
-        //send left and right to the mergeSort to broke it down into pieces
-        //then merge those
-        this.update (this.merge(this.mergeSort(left),this.mergeSort(right)));
-        return this.merge(this.mergeSort(left),this.mergeSort(right));
-    }
-
-    merge(left, right){
-        var result = [],
-            lLen = left.length,
-            rLen = right.length,
-            l = 0,
-            r = 0;
-        while(l < lLen && r < rLen){
-            if(left[l] < right[r]){
-                result.push(left[l++]);
-            }
-            else{
-                result.push(right[r++]);
-            }
+    /*
+        mergeSort(arr){
+            var len = arr.length;
+            if(len <2)
+                return arr;
+            var mid = Math.floor(len/2);
+            var left = arr.slice(0,mid);
+            var right =arr.slice(mid);
+            //send left and right to the mergeSort to broke it down into pieces
+            //then merge those
+            this.update (this.merge(this.mergeSort(left),this.mergeSort(right)));
+            return this.merge(this.mergeSort(left),this.mergeSort(right));
         }
-        //remaining part needs to be addred to the result
-        return result.concat(left.slice(l)).concat(right.slice(r));
-    }*/
+
+        merge(left, right){
+            var result = [],
+                lLen = left.length,
+                rLen = right.length,
+                l = 0,
+                r = 0;
+            while(l < lLen && r < rLen){
+                if(left[l] < right[r]){
+                    result.push(left[l++]);
+                }
+                else{
+                    result.push(right[r++]);
+                }
+            }
+            //remaining part needs to be addred to the result
+            return result.concat(left.slice(l)).concat(right.slice(r));
+        }*/
 
     // sleep time expects milliseconds
-   sleep (time) {
+    sleep (time) {
         return new Promise((resolve) => setTimeout(resolve, time));
     }
 
@@ -174,14 +235,34 @@ export default class Visualiser extends React.Component{
         })
     }
 
-
+/*
     update(array) {
 
         this.setState(state => ({
             data: array
         }));
     }
-
+*/
+    update(array, algorithm){
+        console.log("Algorithm recieved: " + algorithm)
+        switch (algorithm) {
+            case "bubbleSort":
+                this.setState({bubbleSortData: array})
+                break;
+            case "quickSort":
+                console.log("updating quick sort")
+                this.setState({quickSortData: array});
+                break;
+            case "insertionSort":
+                this.setState({insertionSortData: array});
+                break;
+            case "mergeSort":
+                this.setState({mergeSortData: array});
+                break;
+            default:
+                break;
+        }
+    }
 
 
     componentWillUnmount() {
@@ -190,15 +271,45 @@ export default class Visualiser extends React.Component{
 
 
 
+    addAlgorithm(algorithm){
+        let result = this.state.algorithms;
+        if (!(result.includes(algorithm))){
+            result.push(algorithm)
+            this.setState({algorithms: result});
+
+            let data = this.state.data;
+            console.log("state data: " + this.state.data)
+            console.log("data copy: " + data)
+            switch (algorithm){
+                case "quickSort":
+                    this.setState({quickSortData: this.state.data});
+                    break;
+                case "bubbleSort":
+                    this.setState({bubbleSortData: this.state.data});
+                    break;
+                case "insertionSort":
+                    console.log("insertion sort detected")
+                    console.log("data2: " + data)
+                    this.setState({insertionSortData: data});
+                    console.log("data: " + this.state.insertionSortData)
+                    break;
+                case "mergeSort":
+                    this.setState({mergeSortData: this.state.data});
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
 
     loopAlgs(){
-       return(
-           <div>
-               {(this.state.algorithms.map(i => (
-                   [i + ", "]
-               )))}
-           </div>
-       );
+        return(
+            <div>
+                {(this.state.algorithms.map(i => (
+                    this.dataToBars(i)
+                )))}
+            </div>
+        );
     }
 
     render() {
@@ -220,19 +331,22 @@ export default class Visualiser extends React.Component{
                 <p>
                     <div>
                         {(this.state.data.map(i => (
-                        [i + ", "]
+                            [i + ", "]
                         )))}
                     </div>
 
 
-                    </p>
+                </p>
 
 
 
+                <div id = {'algorithms'} className={'array-container'}>
 
-                <div id={'barArea'} className = "array-container">
-                    {this.dataToBars()}
+                    {this.loopAlgs()}
+
                 </div>
+
+
 
             </div>
         );
